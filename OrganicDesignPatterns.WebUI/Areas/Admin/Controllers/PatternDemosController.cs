c23478ebe088f12feacff253f8c65565d4bc2024
SAVAS SEREN <sava.se6565@gmail.com>
2026-06-15 02:43:47 +0300

    Finalize themed WebUI and admin pattern dashboard

---
+++ OrganicDesignPatterns.WebUI/Areas/Admin/Controllers/PatternDemosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrganicDesignPatterns.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class PatternDemosController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
